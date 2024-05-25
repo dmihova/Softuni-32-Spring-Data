847991884f03955568817718cf107e63c6c1a9af
DMIHOVA <desislava.mihova@gmail.com>
2024-05-25 21:56:34 +0300

    v2

--- L07SpringDataAutoMappingObjects/L07ObjectMappingLab/src/main/java/su/l07objectmappinglab/service/AddressService.java
+++
@@ -1,8 +0,0 @@
-package su.l07objectmappinglab.service;
-
-import su.l07objectmappinglab.model.dto.CreateAddressDTO;
-import su.l07objectmappinglab.model.entity.Address;
-
-public interface AddressService {
-    Address create(CreateAddressDTO addresData);
-}
